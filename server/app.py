cf0283344708ad0b0631baa6d45a2c8a39f95d28
Naman <naman.agarwal2019@vitstudent.ac.in>
2021-11-20 18:59:29 +0530

    add svm classifier

--- server/app.py
+++ server/app.py
@@ -2,7 +2,16 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
+import models.Facenet as Facenet
+import db.setup as db
+from server.controllers import user as user_controller
+
+Model = Facenet.load_model()
+con = db.create_connection()
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
@@ -10,9 +19,31 @@ def hello_world():
 
 
 @app.route("/api/face/register", methods=["POST"])
-def register_student():
-    content = request.get_json(silent=True)
-    print(content)
-    return jsonify(
-        status="face registered"
-    )
+def register_face():
+    data = request.get_json(silent=True)
+    res = user_controller.add_user(Model, con, data)
+    if res == True:
+        return jsonify(
+            status="success",
+            msg="Face Registered"    
+        )
+    else:
+        return jsonify(
+            status="error",
+            msg="Something Went Wrong"
+        )
+
+@app.route("/api/face/identify", methods=["POST"])
+def identify_face():
+    data = request.get_json(silent=True)
+    res = user_controller.identify_face(Model, con, data)
+    if res != None:
+        return jsonify(
+            status="success",
+            msg=res    
+        )
+    else:
+        return jsonify(
+            status="error",
+            msg="Not Found"
+        )
